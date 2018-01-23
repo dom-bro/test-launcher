cddc64aefed1944ed1964b535e60667952566b0c
iovec <516911028@qq.com>
2018-01-24 00:47:24 +0800

    shelljs.env——父子进程间通信

--- bin/launcher.js
+++ bin/launcher.js
@@ -1,18 +1,13 @@
 #!/usr/bin/env node
 
 const path = require('path')
-const fs = require('fs')
 const shell = require('shelljs')
 const ROOT = require('../ROOT')
 
+shell.env['cli_args'] = require('./cli-args')
+
 const devServer = path.join(ROOT, 'node_modules/.bin/webpack-dev-server')
-// 由于需要开启子线程运行 webpack-dev-server，因此这里动态生成配置文件
-const devServerConfig = require('../lib/devServer.config')
-fs.writeFile(
-  path.join(ROOT, 'generated.webpack-dev-server.config.js'),
-  JSON.stringify(devServerConfig),
-  err => {
-    if (err) throw err
-    else shell.exec(`${devServer} --config ${ROOT}/generated.webpack-dev-server.config.js`)
-  }
-)
+
+console.log(JSON.stringify(require(`${ROOT}/lib/devServer.config.js`)))
+
+shell.exec(`${devServer} --config ${ROOT}/lib/devServer.config.js`)
